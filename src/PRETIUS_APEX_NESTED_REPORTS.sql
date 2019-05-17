a5e0411178fb41048a8b8c7dfe47e40ebc6ae2e4
Bartosz Ostrowski <ostrowski.bartosz@gmail.com>
2019-05-17 15:54:51 +0200

    Add files via upload

---
+++ src/PRETIUS_APEX_NESTED_REPORTS.sql
@@ -0,0 +1,13 @@
+create or replace package PRETIUS_APEX_NESTED_REPORTS as
+
+  function pretius_row_details (
+    p_dynamic_action in apex_plugin.t_dynamic_action,
+    p_plugin         in apex_plugin.t_plugin 
+  ) return apex_plugin.t_dynamic_action_render_result;
+
+  function pretius_row_details_ajax(
+    p_dynamic_action in apex_plugin.t_dynamic_action,
+    p_plugin         in apex_plugin.t_plugin 
+  ) return apex_plugin.t_dynamic_action_ajax_result;
+
+end;
